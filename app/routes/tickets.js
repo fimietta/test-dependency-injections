ec55771320f3fd6e929c4d7e36fdb07fb431bd6e
fimietta <info@fimietta.it>
2018-07-25 12:05:31 +0100

    Create a tickets route that is using the injected logger factory

---
+++ app/routes/tickets.js
@@ -0,0 +1,10 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+
+  activate() {
+    // The logger property is injected into all routes
+    this.get('logger').log('Entered the tickets route!');
+  },
+
+});
